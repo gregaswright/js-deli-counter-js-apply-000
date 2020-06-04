24d0645c3cffcb3f6e0071c5dbc393e388834833
Gregory A.S. Wright <geslim@gmail.com>
2020-06-04 18:47:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
     array.push(`${i + 1}. ${line[i]}`)
   }
   if (line.length > 0) {
-    return `The line is currently: ${array}`
+    return `The line is currently:${array}`
   } else {
     return 'The line is currently empty.'
   }
